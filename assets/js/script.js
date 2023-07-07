2b4264fa99fb27ef63b37ba4c6d02cad1ee04e90
Sean127 <sdoran401@gmail.com>
2023-07-07 15:23:17 +0000

    javascript changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,31 @@
+const total_amount = 0;
+
 document.addEventListener("DOMContentLoaded", function() {
     document.querySelector('button').addEventListener('click', function() {
         calculateTip();
     });  
 });
 
-function calculateTip() {
+function calculateTip(total_amount) {
     bill_amount = parseInt(document.getElementById("bill_amount").value);
     tip_amount = document.querySelector('input[name="tip_amount"]:checked');
     people = parseInt(document.getElementById("people-tipping").value)
 
-    if (bill_amount === "" || bill_amount < 1 ){
-        alert("Please enter a valid bill amount")
+    console.log(bill_amount,people)
+
+    if (tip_amount.value === "five"){
+        total_amount = bill_amount * 1.05
     }
+    if (tip_amount.value === "ten"){
+        total_amount = bill_amount * 1.1
+    }
+    if (tip_amount.value === "fifteen"){
+        total_amount = bill_amount * 1.15
+    }
+    if (tip_amount.value === "twenty"){
+        total_amount = bill_amount * 1.2
+    }
+
+
+
 }
