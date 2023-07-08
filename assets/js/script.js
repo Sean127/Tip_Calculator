01a832306d0a99f53a7eb74a64dfe9d7b0c04993
Sean127 <sdoran401@gmail.com>
2023-07-08 13:28:06 +0000

    Add functioning javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-const total_amount = 0;
+const total_tip = 0;
 
 document.addEventListener("DOMContentLoaded", function() {
     document.querySelector('button').addEventListener('click', function() {
@@ -6,26 +6,34 @@ document.addEventListener("DOMContentLoaded", function() {
     });  
 });
 
-function calculateTip(total_amount) {
+function calculateTip(total_tip) {
     bill_amount = parseInt(document.getElementById("bill_amount").value);
     tip_amount = document.querySelector('input[name="tip_amount"]:checked');
     people = parseInt(document.getElementById("people-tipping").value)
 
-    console.log(bill_amount,people)
-
     if (tip_amount.value === "five"){
-        total_amount = bill_amount * 1.05
+        total_tip = bill_amount * 0.05
     }
     if (tip_amount.value === "ten"){
-        total_amount = bill_amount * 1.1
+        total_tip = bill_amount * 0.1
     }
     if (tip_amount.value === "fifteen"){
-        total_amount = bill_amount * 1.15
+        total_tip = bill_amount * 0.15
     }
     if (tip_amount.value === "twenty"){
-        total_amount = bill_amount * 1.2
+        total_tip = bill_amount * 0.2
     }
+    let per_person_tip = total_tip/people
+    per_person_tip = per_person_tip.toFixed(2)
+ 
+    if (people > 1){
+        document.getElementById("total_amount").innerHTML = `€${per_person_tip}`
+        document.getElementById("per_person").innerHTML = "Per Person"
 
+    }
+    else{
+        document.getElementById("total_amount").innerHTML = `€${per_person_tip}`
+    }
 
 
 }
