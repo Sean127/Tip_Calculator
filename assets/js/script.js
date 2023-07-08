b49f989bb143f2a475ec4b6f7726ec79f63fde09
Sean127 <sdoran401@gmail.com>
2023-07-08 16:43:05 +0000

    Add CSS styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 const total_tip = 0;
 
-document.addEventListener("DOMContentLoaded", function() {
-    document.querySelector('button').addEventListener('click', function() {
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector('button').addEventListener('click', function () {
         calculateTip();
-    });  
+    });
 });
 
 function calculateTip(total_tip) {
@@ -11,27 +11,27 @@ function calculateTip(total_tip) {
     tip_amount = document.querySelector('input[name="tip_amount"]:checked');
     people = parseInt(document.getElementById("people-tipping").value)
 
-    if (tip_amount.value === "five"){
+    if (tip_amount.value === "five") {
         total_tip = bill_amount * 0.05
     }
-    if (tip_amount.value === "ten"){
+    if (tip_amount.value === "ten") {
         total_tip = bill_amount * 0.1
     }
-    if (tip_amount.value === "fifteen"){
+    if (tip_amount.value === "fifteen") {
         total_tip = bill_amount * 0.15
     }
-    if (tip_amount.value === "twenty"){
+    if (tip_amount.value === "twenty") {
         total_tip = bill_amount * 0.2
     }
-    let per_person_tip = total_tip/people
+    let per_person_tip = total_tip / people
     per_person_tip = per_person_tip.toFixed(2)
- 
-    if (people > 1){
+    document.getElementById("tip_total").innerHTML = "The total tip amount is"
+
+    if (people > 1) {
         document.getElementById("total_amount").innerHTML = `€${per_person_tip}`
         document.getElementById("per_person").innerHTML = "Per Person"
 
-    }
-    else{
+    } else {
         document.getElementById("total_amount").innerHTML = `€${per_person_tip}`
     }
 
